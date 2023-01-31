baf605e7bfea86c19e9fdb62f5190e5e41b63525
Matt Enyeart <menyeart1@gmail.com>
2023-01-31 15:16:53 -0700

    Add the national parks controller show method

--- app/controllers/national_parks_controller.rb
+++ app/controllers/national_parks_controller.rb
@@ -2,4 +2,8 @@ class NationalParksController < ApplicationController
   def index
     @national_parks = NationalPark.all
   end
+
+  def show
+    @national_park = NationalPark.find(params[:id])
+  end
 end
