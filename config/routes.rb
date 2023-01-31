cf0732666aad24e2c72a16047eac78cc69e959ac
Matt Enyeart <menyeart1@gmail.com>
2023-01-31 15:18:30 -0700

    Add national parks get :id route and show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/national_parks', to: 'national_parks#index'
+  get '/national_parks/:id', to: 'national_parks#show'
+
 end
