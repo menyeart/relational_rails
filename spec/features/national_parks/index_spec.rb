cb2a503067b278291934aa7cb8d9edc93351e69a
Matt Enyeart <menyeart1@gmail.com>
2023-02-01 14:53:05 -0700

    Consolidate multiple test for each attribute into one test with several expects

--- spec/features/national_parks/index_spec.rb
+++ spec/features/national_parks/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'the national parks index page' do
+RSpec.describe 'the national parks index page', type: :feature do
   it 'displays the names of every national park' do
     national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
     national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
