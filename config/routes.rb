dd06eeace905191bae30ceb749a753e5c0511fe9
Matt Enyeart <menyeart1@gmail.com>
2023-02-01 20:11:41 -0700

    Add route for natoinal park trails index

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/national_parks/:id', to: 'national_parks#show'
   get '/trails', to: 'trails#index'
   get 'trails/:id', to: 'trails#show'
+  get '/national_parks/:national_park_id/trails', to: 'national_park_trails#index'
 
 end
