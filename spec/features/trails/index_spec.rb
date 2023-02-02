1d9850e67d47f55f88c632a9ddacf45d1ffd6b91
Matt Enyeart <menyeart1@gmail.com>
2023-02-01 19:22:01 -0700

    Refactor features trails index spec to account for has many relationship with national parks

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'the trails index page' do
   it "displays the name of every trail" do
-    trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true)
-    trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false)
+    national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+    trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true, national_park_id: national_park1.id)
+    trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false, national_park_id: national_park1.id)
   
     visit "/trails/"
 
