b4fc59579bc00636c7951f47450d6ece278c6b6c
Matt Enyeart <menyeart1@gmail.com>
2023-02-01 18:16:23 -0700

    Modify trails index spec to account for spelling correction

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe 'the trails index page' do
     visit "/trails/"
 
     expect(page).to have_content(trail.name)
-    expect(page).to have_content("Total Distance (miles): #{trail.length_miles}")
+    expect(page).to have_content("Total Distance(miles): #{trail.length_miles}")
     expect(page).to have_content("Elevation Gain(ft): #{trail.feet_elevation_gain}")
     expect(page).to have_content(trail2.name)
-    expect(page).to have_content("Total Distance (miles): #{trail2.length_miles}")
+    expect(page).to have_content("Total Distance(miles): #{trail2.length_miles}")
     expect(page).to have_content("Elevation Gain(ft): #{trail2.feet_elevation_gain}")
   end
 end
