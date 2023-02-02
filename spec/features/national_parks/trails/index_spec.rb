e64985d3afe24056077a2b1cbee472978f0675db
Matt Enyeart <menyeart1@gmail.com>
2023-02-02 14:22:37 -0700

    Refactor test for more specificity

--- spec/features/national_parks/trails/index_spec.rb
+++ spec/features/national_parks/trails/index_spec.rb
@@ -9,15 +9,8 @@ RSpec.describe "National Park's trails index" do
   visit "/national_parks/#{glacier.id}/trails"
 
   expect(page).to have_content(highline.name)
+  expect(page).to have_content("Total Distance(miles): #{highline.length_miles}")
   expect(page).to have_content(grinell.name)
+  expect(page).to have_content("Total Distance(miles): #{grinell.length_miles}")
  end
-
-
-#  it "shows all each trails attributes" do
-#   glacier = NationalPark.create!(name: 'Glacier', state: 'Montana', total_acres: 1012837, charges_fee: true)
-#   highline = glacier.trails.create!(name: 'Highline', length_miles: 15, water_source: false, feet_elevation_gain: 2600)
-#   grinell = glacier.trails.create!(name: 'Grinell Glacier', length_miles: 11, water_source: true, feet_elevation_gain: 2181)
-
-#   visit "/national_parks/#{glacier.id}/trails"
-#  end
 end
