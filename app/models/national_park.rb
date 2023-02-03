1bdf0a76478d09ff75858e7f133dde1f40e94b6f
Matt Enyeart <menyeart1@gmail.com>
2023-02-02 17:53:54 -0700

    Add order by most recent method

--- app/models/national_park.rb
+++ app/models/national_park.rb
@@ -2,4 +2,9 @@ class NationalPark < ApplicationRecord
   has_many :trails
 
   validates_presence_of :name
+
+  def self.order_by_most_recent
+    NationalPark.order(created_at: :desc)
+  end
+
 end
