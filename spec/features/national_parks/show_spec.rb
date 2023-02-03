fd795883ecc56b753b061503ee6360ec96759232
Matt Enyeart <menyeart1@gmail.com>
2023-02-03 15:56:56 -0700

    Remove unneccessary object creation from tests

--- spec/features/national_parks/show_spec.rb
+++ spec/features/national_parks/show_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe 'the national parks show page' do
 
   it "it displays links for the national parks index page and trails index page" do
     national_park = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
-    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
 
     visit "/national_parks/#{national_park.id}"
 
@@ -27,11 +26,10 @@ RSpec.describe 'the national parks show page' do
 
   it "it displays the link to its child_table_name` page " do
     national_park = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
-    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
 
     visit "/national_parks/#{national_park.id}"
 
-    expect(page).to have_link "All trails of Rocky Mountain", href: "/national_parks/#{national_park.id}/trails"
+    expect(page).to have_link "All trails of #{national_park.name}", href: "/national_parks/#{national_park.id}/trails"
   end
 
 
