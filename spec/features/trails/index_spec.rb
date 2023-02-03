c047b93267e1c41ffd566c05237c4e2b75e67c04
Matt Enyeart <menyeart1@gmail.com>
2023-02-02 19:28:29 -0700

    Add test for national parks and trails index links

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe 'the trails index page' do
     expect(page).to have_content("Total Distance(miles): #{trail2.length_miles}")
     expect(page).to have_content("Elevation Gain(ft): #{trail2.feet_elevation_gain}")
   end
+
+  it "it displays links for the national parks index page and trails index page" do
+      national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+      national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+      trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true, national_park_id: national_park1.id)
+      trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false, national_park_id: national_park1.id)
+    
+      visit "/trails/"
+
+      expect(page).to have_link 'All National Parks', href: '/national_parks'
+      expect(page).to have_link 'All Trails', href: '/trails'
+  end
+
 end
