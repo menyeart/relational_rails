453cbe9b74394318dbc03ce619e738e7bb52b4e0
Matt Enyeart <menyeart1@gmail.com>
2023-02-02 17:56:14 -0700

    Add national parks sorted instance variable

--- app/controllers/national_parks_controller.rb
+++ app/controllers/national_parks_controller.rb
@@ -1,6 +1,7 @@
 class NationalParksController < ApplicationController
   def index
     @national_parks = NationalPark.all
+    @national_parks_sorted = NationalPark.order_by_most_recent
   end
 
   def show
