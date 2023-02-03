10be5dec0dd081389fa5a1b78101f5c1f1e3dc23
Matt Enyeart <menyeart1@gmail.com>
2023-02-02 19:27:56 -0700

    Add test for national parks and trails index links

--- spec/features/national_parks/show_spec.rb
+++ spec/features/national_parks/show_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe 'the national parks show page' do
     expect(page).to_not have_content("Total Acres: #{national_park2.total_acres}")
   end
 
+  it "it displays links for the national parks index page and trails index page" do
+    national_park = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+
+    visit "/national_parks/#{national_park.id}"
+
+    expect(page).to have_link 'All National Parks', href: '/national_parks'
+    expect(page).to have_link 'All Trails', href: '/trails'
+  end
+
 end
