4d1668a9d68e8dcd1f91c665fbedf691c215c58a
Matt Enyeart <menyeart1@gmail.com>
2023-02-05 12:00:45 -0700

    Add update method and trails params private method

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -6,4 +6,21 @@ class TrailsController < ApplicationController
   def show
     @trail = Trail.find(params[:id])
   end
+
+  def edit
+    @trail = Trail.find(params[:id])
+  end
+
+  def update
+    trail = Trail.find(params[:id])
+    trail.update(trail_params)
+    redirect_to "/trails/#{trail.id}"
+
+  end
+
+  private
+    def trail_params
+      params.permit(:name, :length_miles, :feet_elevation_gain, :water_source)
+    end
+
 end
