fdd0036416d9ef5fc43dca9bd009d664121f8bef
Matt Enyeart <menyeart1@gmail.com>
2023-02-05 11:07:00 -0700

    Add create method and national park trails params private method

--- app/controllers/national_park_trails_controller.rb
+++ app/controllers/national_park_trails_controller.rb
@@ -3,4 +3,19 @@ class NationalParkTrailsController < ApplicationController
    @national_park = NationalPark.find(params[:national_park_id])
    @trails = @national_park.trails
   end
+
+  def new
+    @national_park = NationalPark.find(params[:id])
+  end
+
+  def create
+    @national_park = NationalPark.find(params[:id])
+    Trail.create(national_park_trails_params)
+    redirect_to "/national_parks/#{@national_park.id}/trails"
+  end
+
+  private
+  def national_park_trails_params
+    params.permit(:name, :length_miles, :feet_elevation_gain, :water_source, :national_park_id)
+  end
 end
