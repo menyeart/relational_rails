1c83512ba2534d2ee8d9dc9b19e3150f9d6dccb0
Matt Enyeart <menyeart1@gmail.com>
2023-02-06 19:01:24 -0700

    Refactor national parks index spec with before each block

--- spec/features/national_parks/index_spec.rb
+++ spec/features/national_parks/index_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'the national parks index page', type: :feature do
-  it 'displays the names of every national park' do
-    national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
-    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+  before :each do
+    @national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+    @national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+  end  
 
+  it 'displays the names of every national park' do
     visit "/national_parks"
 
-    expect(page).to have_content(national_park1.name)
-    expect(page).to have_content(national_park2.name)
+    expect(page).to have_content(@national_park1.name)
+    expect(page).to have_content(@national_park2.name)
   end
 
   it " displays links for the national parks index page and trails index page" do
-    national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
-    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
-
     visit "/national_parks"
 
     expect(page).to have_link 'All National Parks', href: '/national_parks'
