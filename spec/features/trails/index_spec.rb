8023bfc4b2e0ac5185948e92e99ef952e5dddf6b
Matt Enyeart <menyeart1@gmail.com>
2023-02-06 18:21:39 -0700

    Add test for trails index button to only show records with water source true

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -18,15 +18,29 @@ RSpec.describe 'the trails index page' do
   end
 
   it "it displays links for the national parks index page and trails index page" do
-      national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
-      national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
-      trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true, national_park_id: national_park1.id)
-      trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false, national_park_id: national_park1.id)
+    national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+    national_park2 = NationalPark.create!(name: "Yosemite", state: "California", total_acres: 10000000, charges_fee: true)
+    trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true, national_park_id: national_park1.id)
+    trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false, national_park_id: national_park1.id)
     
-      visit "/trails/"
+    visit "/trails/"
 
-      expect(page).to have_link 'All National Parks', href: '/national_parks'
-      expect(page).to have_link 'All Trails', href: '/trails'
+    expect(page).to have_link 'All National Parks', href: '/national_parks'
+    expect(page).to have_link 'All Trails', href: '/trails'
+  end
+
+  it "only shows true or false records when the selector is chosen" do
+    national_park1 = NationalPark.create!(name: "Rocky Mountain", state: "Colorado", total_acres: 265807, charges_fee: true)
+    trail = Trail.create!(name: "Longs Peak", length_miles: 14, feet_elevation_gain: 4960, water_source: true, national_park_id: national_park1.id)
+    trail2 = Trail.create!(name: "Mosca Pass", length_miles: 6,feet_elevation_gain: 1400, water_source: false, national_park_id: national_park1.id)
+    
+    visit '/trails/'
+    click_button('Show only trails with water')
+
+    expect(current_path).to eq("/trails/")
+    expect(page).to have_content("Longs Peak")
+    expect(page).to_not have_content("Mosca Pass")
+    expect(page).to_not have_content("Water Source: false")
   end
 
 end
