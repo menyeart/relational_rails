75b40abd58310dfd015001febbf043d01b6847cf
Matt Enyeart <menyeart1@gmail.com>
2023-02-08 15:59:56 -0700

    Add test in national parks trails index spec for new sort form

--- spec/features/national_parks/trails/index_spec.rb
+++ spec/features/national_parks/trails/index_spec.rb
@@ -49,5 +49,24 @@ RSpec.describe "National Park's trails index" do
       
       expect(current_path).to eq("/trails/#{grinell.id}/edit")
     end
+
+    it "has a form to input a number value and get records that match that specification" do
+      glacier = NationalPark.create!(name: 'Glacier', state: 'Montana', total_acres: 1012837, charges_fee: true)
+      highline = glacier.trails.create!(name: 'Highline', length_miles: 15, water_source: false, feet_elevation_gain: 2600)
+      grinell = glacier.trails.create!(name: 'Grinell Glacier', length_miles: 5, water_source: true, feet_elevation_gain: 2181)
+      swiftcurernt = glacier.trails.create!(name: 'Swiftcurrent Peak', length_miles: 2, water_source: true, feet_elevation_gain: 4000)
+
+      visit "/national_parks/#{glacier.id}/trails"
+      
+      fill_in('Find only trails with lengths over:', with: 5 )
+   
+      click_button("Search")
+   
+      expect(current_path).to eq("/national_parks/#{glacier.id}/trails")
+      expect(page).to have_content("Highline")
+      expect(page).to_not have_content("Grinell Glacier")
+      expect(page).to_not have_content("Swiftcurrent Peak")
+
+    end
   end
 end
